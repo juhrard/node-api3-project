1525553bfde7d7e21f28d60c0514f403bd5cdb03
Gerard Crockenberg <gmahred@gmail.com>
2020-02-12 17:37:18 -0500

    finished base project

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,92 @@
 const express = require('express');
+const Posts = require('./postDb');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  console.log("headers", req.headers);
+
+  Posts.get(req.query)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the Posts",
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, (req, res) => {
+  Posts.getById(req.params.id)
+  .then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "post not found" });
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      message: "Error retrieving the post",
+    });
+  });
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  Posts.remove(req.params.id)
+  .then(count => {
+    if (count > 0) {
+      res.status(200).json({ message: "The post has been nuked" });
+    } else {
+      res.status(404).json({ message: "The post could not be found" });
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      message: "Error removing the post",
+    });
+  });
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  Posts.update(req.params.id, req.body)
+  .then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "The post could not be found" });
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      message: "Error updating the post",
+    });
+  });
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  Posts.getById(req.params.id)
+  .then(post => {
+    if(post) {
+      req.post = post;
+      next();
+    } else {
+      res.status(400).json({ message: "Invalid post id" })
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      message: "Error retreiving the user",
+    });
+  });
 }
 
 module.exports = router;
